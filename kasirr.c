6010392f9aae42eafaddb7f46ea74a6971a8dce6
Ssurryaa <75293376+Ssurryaa@users.noreply.github.com>
2020-12-18 14:28:13 +0700

    Update kasirr.c

--- kasirr.c
+++ kasirr.c
@@ -2,7 +2,7 @@
 #include <time.h> // Header dalam C untuk memanipulasi waktu
 
 int h1, h2, h3, h4, h5;
-int j=0, j2=0, j3=0, j4=0, j5=0;
+int j1=0, j2=0, j3=0, j4=0, j5=0;
 int t1=0, t2=0, t3=0, t4=0, t5=0;
 int m1, m2, m3, m4, m5;
 int j6=0, j7=0, j8=0, j9=0, j10=0;
@@ -33,8 +33,9 @@ void tentangKami();
 float mendapatkanDiskon();
 float potonganHarga();
 float hargaAkhir();
-float transaksiAkhir();
-float struk();
+float transaksiPembayaran();
+float transaksiKembalian();
+float laporanTransaksi();
 void tentangKami();
 
 
@@ -141,8 +142,7 @@ void loginAdmin()
 // A.A. Krisnha Wirayudha - 2005551087                                   //
 // Kelas B                                                               //
 //=======================================================================//
-float daftarMenu()
-{
+float daftarMenu(){
     int pilihan;
     int makanan;
     int minuman;
@@ -183,8 +183,8 @@ float daftarMenu()
             case 1 :
                     h1=6500;
                     printf (" ==> Nasi Telur \t: @ Rp.%d\n", h1);
-                    printf (" Jumlah pesanan \t: "); scanf("%d", &j);
-                    t1=j*h1;
+                    printf (" Jumlah pesanan \t: "); scanf("%d", &j1);
+                    t1=j1*h1;
                     break;
 
             case 2 :
@@ -363,8 +363,8 @@ float daftarMenu()
      hasil :
         printf("\nPesanan Anda :\n");
 
-        if(j>0){
-            printf("Nasi Telur : %d\n",j);
+        if(j1>0){
+            printf("Nasi Telur : %d\n",j1);
         }else{}
 
         if(j2>0){
@@ -428,7 +428,7 @@ float daftarMenu()
         printf("Harga : %d\n", total);
         printf("====================\n");
 
-    return total;
+    mendapatkanDiskon();
 }
 //=============================Fungsi Diskon=============================//
 //=======================================================================//
@@ -545,8 +545,6 @@ float transaksiKembalian(){
     printf("+=======================================================================+\n");
 
 laporanTransaksi();
-    
-return 0;
 }
 
 //==========================Fungsi Struk Pesanan=========================//
@@ -568,7 +566,7 @@ return 0;
 // Kelas B                                                               //
 //=======================================================================//
 
-float struk(){
+float laporanTransaksi(){
 
     FILE *fptr;
 
@@ -584,8 +582,8 @@ float struk(){
     fprintf(fPointer,"Nama Pesanan\t | Jumlah\t | Harga\t | Total\t|\n");
     fprintf(fPointer,"=================================================================\n");
 
-   if(j>0){
-           fprintf(fPointer,"Nasi Telur\t | %d\t\t | Rp.%d\t | Rp.%d\t|\n", j, h1, t1);
+   if(j1>0){
+           fprintf(fPointer,"Nasi Telur\t | %d\t\t | Rp.%d\t | Rp.%d\t|\n", j1, h1, t1);
         }else{}
 
         if(j2>0){
@@ -664,8 +662,8 @@ float struk(){
     fprintf(fPointer,"Nama Pesanan\t | Jumlah\t | Harga\t | Total\t|\n");
     fprintf(fPointer,"=================================================================\n");
 
-     if(j>0){
-           fprintf(fPointer,"Nasi Telur\t | %d\t\t | Rp.%d\t | Rp.%d\t|\n", j, h1, t1);
+     if(j1>0){
+           fprintf(fPointer,"Nasi Telur\t | %d\t\t | Rp.%d\t | Rp.%d\t|\n", j1, h1, t1);
         }else{}
 
         if(j2>0){
